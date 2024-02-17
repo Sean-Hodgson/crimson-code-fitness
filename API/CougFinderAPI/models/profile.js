d63f7d5857c8fec1ce67fad8f43123b6574389c9
David Tran <david.tran1@wsu.edu>
2024-02-17 13:46:45 -0800

    Made changes to sign in feature

--- API/CougFinderAPI/models/profile.js
+++ API/CougFinderAPI/models/profile.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const profileSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: String,
-    instagramTag: String,
-    snapchatTag: String,
-    favSong: String,
-    Major: String,
-    interests: [
-        
+    Name: String,
+    email: String,
+    weight: String,
+    Height: String,
+    Interests: [
+
     ]
+    
 });
 
 module.exports = mongoose.model('Profile', profileSchema);
